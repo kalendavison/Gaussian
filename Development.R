6d5e9b1daf242f07520bfe750b488df8a6ed3979
Kalen Davison <davison@wustl.edu>
2018-03-24 13:27:04 -0500

    Setting up shared GitHub Repo

---
+++ Development.R
@@ -0,0 +1,45 @@
+library(devtools)
+install_github('goldingn/gpe')
+library(gpe)
+?gp
+
+getwd()
+setwd("/Users/kalendavison/Desktop/Applied Statistical Programming")
+vote_data = read.delim("votingdata.dat")
+mean(vote_data$rvote, na.rm = TRUE)
+output = lm(rvote ~ eth, data = vote_data)
+output # y=.62695-.09266x  
+
+# Create an rbf kernel which acts on some variable named temperature
+k1 <- rbf('temperature')
+# look at the parameters
+summary(k1)
+# plot covariance
+plot(k1)
+# look at some GPs drawn from this kernel
+demoKernel(k1)
+
+# make a fake 'true' function
+f <- function(x) 2 * sin(x)
+
+# make a fake dataset
+x <- sort(runif(100, -2, 2))
+y <- rpois(100, exp(f(x)))
+df <- data.frame(y, x)
+
+# fit a Poisson GP model with an rbf kernel
+m <- gp(y ~ rbf('x'), data = df, family = poisson)
+
+# predict from it
+pred_df <- data.frame(x = seq(min(df$x), max(df$x), len = 500))
+lambda <- predict(m, pred_df, type = 'response')
+
+# plot the predicted rate parameter, the true model and the data
+plot(lambda ~ pred_df$x, type = 'l', lwd = 2, ylim = range(y))
+lines(exp(f(pred_df$x)) ~ pred_df$x, lty = 2)
+points(y ~ x, data = df)
+
+# note you can get the posterior variance (prediction uncertainty) too,
+# just set 'sd = TRUE' when predicting
+
+gp(formula = )
