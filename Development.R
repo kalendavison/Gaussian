b1a66aef15dd8ce70201c38a7d44c8074998514b
Kalen Davison <davison@wustl.edu>
2018-03-27 13:39:12 -0500

    Newest version

--- Development.R
+++ Development.R
@@ -10,7 +10,6 @@ mean(vote_data$rvote, na.rm = TRUE)
 
 
 vote_data = na.exclude(vote_data) #cleaning of all missing data
-View(vote_data)
 summary(vote_data$stt)
 
 state.f<-factor(vote_data$stt)
@@ -23,33 +22,39 @@ vote_data$api<-ifelse(vote_data$eth==4, c(1), c(0))
 
 vote_data<-cbind(vote_data, dummies)
 
-summary(vote_data$state.f5)
+summary(vote_data$state.f25)
 
-votedata5<-subset(vote_data, vote_data$state.f5==1)
-View(votedata5)
+votedata25<-subset(vote_data, vote_data$state.f25==1)
 
-mean(votedata5$rvote[votedata5$eth == 1], na.rm = TRUE) #white mean republican vote proportion
-mean(votedata5$rvote[votedata5$eth == 2], na.rm = TRUE) #black
-mean(votedata5$rvote[votedata5$eth == 3], na.rm = TRUE) #asian/hispanic
-mean(votedata5$rvote[votedata5$eth == 4], na.rm = TRUE) #asian/hispanic
+mean(votedata25$rvote[votedata25$eth == 1], na.rm = TRUE) #white mean republican vote proportion
+mean(votedata25$rvote[votedata25$eth == 2], na.rm = TRUE) #black
+mean(votedata25$rvote[votedata25$eth == 3], na.rm = TRUE) #asian/hispanic
+mean(votedata25$rvote[votedata25$eth == 4], na.rm = TRUE) #asian/hispanic
 
-output = lm(rvote ~ eth, data = votedata5) #we need to make dummy variables for ethnicity to isolate its effect
-output = lm(rvote ~ white, data = votedata5)
+output = lm(rvote ~ eth, data = votedata25) #we need to make dummy variables for ethnicity to isolate its effect
+output = lm(rvote ~ white, data = votedata25)
 output
-output = lm(rvote ~ white + sex, data = votedata5)
+output = lm(rvote ~ white + sex, data = votedata25)
 output #being white has a stronger affect on voting republican
 
-vote.df5<-as.data.frame(votedata5)
-vote.df5.reduced<-vote.df5[,c("rvote", "white")]
+vote.df25<-as.data.frame(votedata25)
+vote.df25.reduced<-vote.df25[,c("rvote", "white")]
 
-output<-gp(formula = rvote~rbf("white"), data = vote.df5.reduced , family = binomial)
+head(vote.df25.reduced)
+rbf(vote.df25.reduced$white)
 
-plot(output$posterior$components$a, vote.df5.reduced$rvote)
+output<-gp(formula = rvote~rbf("white"), data = vote.df25.reduced, family = binomial)
+
+summary(output)
+dim(vote.df25.reduced)
 
 
-my.prediction<-predict(output, vote.df5, type="response")
-plot(my.prediction, vote.df5$white)
-vote.df5$rvote
+plot(output$posterior$components$a, vote.df25.reduced$rvote)
 
-as.data.frame(votedata5$rvote)
+
+my.prediction<-predict(output, vote.df25, type="response")
+plot(my.prediction, vote.df25$white)
+vote.df25$rvote
+
+as.data.frame(votedata25$rvote)
 
