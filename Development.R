437da662fb2d3c835f0dfe1dbdc485111d0d7b6a
Noah Bardash <noah.bardash@wustl.edu>
2018-03-27 14:14:24 -0500

    male & female dummies

--- Development.R
+++ Development.R
@@ -1,6 +1,9 @@
+rm(list = ls())
+
 library(devtools)
 install_github('goldingn/gpe')
 library(gpe)
+install.packages('lme4')
 ?gp
 
 getwd()
@@ -21,6 +24,9 @@ vote_data$black<-ifelse(vote_data$eth==2, c(1), c(0))
 vote_data$hisp<-ifelse(vote_data$eth==3, c(1), c(0))
 vote_data$api<-ifelse(vote_data$eth==4, c(1), c(0))
 
+vote_data$male<-ifelse(vote_data$sex==1, c(1), c(0))
+vote_data$female<-ifelse(vote_data$sex==2, c(1), c(0))
+
 vote_data<-cbind(vote_data, dummies)
 
 summary(vote_data$state.f25)
@@ -31,6 +37,8 @@ mean(votedata25$rvote[votedata25$eth == 1], na.rm = TRUE) #white mean republican
 mean(votedata25$rvote[votedata25$eth == 2], na.rm = TRUE) #black
 mean(votedata25$rvote[votedata25$eth == 3], na.rm = TRUE) #asian/hispanic
 mean(votedata25$rvote[votedata25$eth == 4], na.rm = TRUE) #asian/hispanic
+mean(votedata25$rvote[votedata25$sex == 1], na.rm = TRUE) #male republican vote proportion
+mean(votedata25$rvote[votedata25$sex == 2], na.rm = TRUE) #female
 
 output = lm(rvote ~ eth, data = votedata25) #we need to make dummy variables for ethnicity to isolate its effect
 output = lm(rvote ~ white, data = votedata25)
@@ -39,9 +47,16 @@ output = lm(rvote ~ white + sex, data = votedata25)
 output #being white has a stronger affect on voting republican
 
 vote.df25<-as.data.frame(votedata25)
-vote.df25.reduced<-vote.df25[,c("rvote", "white")]
+head(vote.df25)
+View(vote.df25)
+vote.df25.reduced<-vote.df25[,c("rvote", "white", "sex")]
+head(vote.df25.reduced)
 output<-gp(formula = rvote~rbf("white"), data = vote.df25.reduced, family = binomial)
 plot(output$posterior$components$a, vote.df25.reduced$rvote)
+?rbf
+
+test <- expand.grid(vote.df25.reduced$white, vote.df25.reduced$male)
+test
 
 my.prediction<-predict(output, vote.df25, type="response")
 plot(my.prediction, vote.df25$white)
