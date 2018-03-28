379d462ddc0bbc8360dcb276971f15ffcc96cf18
Kalen Davison <davison@wustl.edu>
2018-03-28 11:17:50 -0500

    update

--- Development.R
+++ Development.R
@@ -28,7 +28,7 @@ vote_data$white<-ifelse(vote_data$eth==1, c(1), c(0))
 vote_data$black<-ifelse(vote_data$eth==2, c(1), c(0))
 vote_data$hisp<-ifelse(vote_data$eth==3, c(1), c(0))
 vote_data$api<-ifelse(vote_data$eth==4, c(1), c(0))
-vote_data$sex<-ifelse(vote_data$sex==1, c(1), c(0)) #recode sex to 0 1 dummy instead of 1 2
+vote_data$man<-ifelse(vote_data$sex==1, c(1), c(0)) #recode sex to 0 1 dummy instead of 1 2
 vote_data$mar<-ifelse(vote_data$mar==1, c(1), c(0)) #recode married to 0 1
 vote_data$kid<-ifelse(vote_data$kid==1, c(1), c(0)) #recode kid to 0 1 
 votedata25<-subset(vote_data, vote_data$state.f25==1) #using only 25th state for now
@@ -49,8 +49,8 @@ output #being white has a stronger affect on voting republican
 
 #using gp function to do analysis
 vote.df25<-as.data.frame(votedata25)
-vote.df25.reduced<-vote.df25[,c("rvote", "white", "sex", "mar", "kid")]
-output<-gp(formula = rvote~rbf(c("white", "sex")), data = vote.df25.reduced, family = binomial) ### compare output of this with lmer output. see pdf on doc for assistance
+vote.df25.reduced<-vote.df25[,c("rvote", "white", "man", "mar", "kid")]
+output<-gp(formula = rvote~rbf(c("white", "man")), data = vote.df25.reduced, family = binomial) ### compare output of this with lmer output. see pdf on doc for assistance
 my.prediction<-predict(output, vote.df25, type="response")
 table(my.prediction) #there are four possible probabilities of voting republican (associated with white male, nonwhite male, white female, nonwhite female)
 plot(output$posterior$components$a, vote.df25.reduced$rvote)
@@ -59,7 +59,7 @@ plot(my.prediction, vote.df25$white)
 
 #working with glmer function
 var1 = as.factor(vote.df25.reduced$white)
-var2 = as.factor(vote.df25.reduced$sex)
+var2 = as.factor(vote.df25.reduced$man)
 var3 = as.factor(vote.df25.reduced$mar)
 check = glmer(formula = rvote ~ (1|kid) + var1 + var2, data = vote.df25.reduced, family = binomial) #may need more variables (white man...nonwhite woman)
 display(check) 
