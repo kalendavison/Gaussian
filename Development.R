c71f319ab5942811888804c41746aec2211c6746
Noah Bardash <noah.bardash@wustl.edu>
2018-04-15 10:25:27 -0500

    cleaning

--- Development.R
+++ Development.R
@@ -20,132 +20,6 @@ setwd("/Users/kalendavison/Desktop/Applied Statistical Programming/GitHub/Gaussi
 setwd("/Users/Ian Davis/Documents/GitHub/Gaussian")
 setwd("/Users/noahbardash/Documents/GitHub/Gaussian")
 
-<<<<<<< HEAD
-# Reading in & processing of data
-vote_data = read.delim("votingdata.dat") # Read in dataset from .dat file
-vote_data = na.exclude(vote_data) # Remove all entries with missing data
-
-# Clean dataset: state 2 (AK) has no entries and state 12 (HI) only has one. No statistically useful data here
-vote_data <- vote_data[!(vote_data$stt==12),] # Removal of Hawaii entry from dataset
-vote_data$stt <- ifelse(vote_data$stt > 2, vote_data$stt - 1, vote_data$stt) # Recode stt value for states alphabetically after AK
-vote_data$stt <- ifelse(vote_data$stt > 12, vote_data$stt - 1, vote_data$stt) # Recode stt values for states alphabetically after HI
-# 48 contiguous states + DC now numbered 1-49 in alphabetical order
-
-# Creation of dummy variables for each state
-state.f<-factor(vote_data$stt)
-dummies<-model.matrix(~state.f)
-vote_data<-cbind(vote_data, dummies) # Attach dummy matrix to vote_data
-
-# Creation of dummy variables for each race
-vote_data$white<-ifelse(vote_data$eth==1, c(1), c(0))
-vote_data$black<-ifelse(vote_data$eth==2, c(1), c(0))
-vote_data$hisp<-ifelse(vote_data$eth==3, c(1), c(0))
-vote_data$api<-ifelse(vote_data$eth==4, c(1), c(0))
-
-# Creation of dummy variables for each sex 
-vote_data$male <- ifelse(vote_data$sex==1, c(1), c(0))
-vote_data$female <- ifelse(vote_data$sex==2, c(1), c(0))
-
-# Creation of dummy variables for each ethnicity
-vote_data$noHS <- ifelse(vote_data$edu==1, c(1), c(0))
-vote_data$HSgrad <- ifelse(vote_data$edu==2, c(1), c(0))
-vote_data$somecollege <- ifelse(vote_data$edu==3, c(1), c(0))
-vote_data$bachelors <- ifelse(vote_data$edu==4, c(1), c(0))
-vote_data$adv_degree <- ifelse(vote_data$edu==5, c(1), c(0))
-
-# ((possibly working with these variables later))
-vote_data$mar<-ifelse(vote_data$mar==1, c(1), c(0)) # Recode married to 0 1
-vote_data$kid<-ifelse(vote_data$kid==1, c(1), c(0)) # Recode kid to 0 1 
-
-# State data subsets and cast as dataframes
-votedata23<-subset(vote_data, vote_data$state.f23==1) # Mississippi subset
-vote.df23<-as.data.frame(votedata23)
-votedata2<-subset(vote_data, vote_data$state.f2==1) # Arizona subset
-vote.df2<-as.data.frame(votedata2)
-votedata20<-subset(vote_data, vote_data$state.f20==1) # Massachusetts subset
-vote.df20<-as.data.frame(votedata20)
-vote.df<-as.data.frame(vote_data) # Full dataset cast as dataframe
-
-
-### GP FUNCTION ###
-
-# Run GP function for MS, AZ, MA, full dataset
-output_miss<-gp(formula = rvote~rbf(c("sex", "edu", "eth")), data = vote.df23, family = binomial)
-output_ariz<-gp(formula = rvote~rbf(c("sex", "edu", "eth")), data = vote.df2, family = binomial)
-output_mass<-gp(formula = rvote~rbf(c("sex", "edu", "eth")), data = vote.df20, family = binomial)
-output_mass2<-gp(formula = rvote~rbf(c("sex", "edu", "eth"), sigma = 1, l = c(1,1,2)), data = vote.df20, family = binomial)
-rm(list=setdiff(ls(), "vote.df")) #put this in to potentially help with the other function; doesnt work 
-output_all<-gp(formula = rvote~rbf(c("sex", "edu", "eth", "stt")), data = vote.df, family = binomial)
-
-# Creation of a fake dataset for each unique demographic combination 
-eth = c(rep(1,10), rep(2,10), rep(3,10), rep(4,10))
-sex = c(rep((c(rep(1,5), rep(2,5))), 4))
-edu = rep(1:5, 8)
-fake.dataset = data.frame(eth, sex, edu) # Cast fake dataset as dataframe
-
-# Run GP function for each of the demographic subgroups to obtain 40 predicted R-vote values
-predictions_miss<-predict(output_miss, fake.dataset, type="response")
-predictions_ariz<-predict(output_ariz, fake.dataset, type="response")
-predictions_mass<-predict(output_mass, fake.dataset, type="response")
-
-# Display R-vote predictions alongside demographic categories in a dataframe
-demographic.prediction.MS = data.frame(predictions_miss, fake.dataset)
-View(demographic.prediction.MS) 
-demographic.prediction.AZ = data.frame(predictions_ariz, fake.dataset)
-View(demographic.prediction.AZ) 
-demographic.prediction.MA = data.frame(predictions_mass, fake.dataset)
-View(demographic.prediction.MA)
-
-
-### GLMER FUNCTION ###
-
-
-# Make a fake dataset for each unique demographic combination using dummy variables
-white <- c(rep(1,10), rep(0,30))
-black <- c(rep(0,10), rep(1,10), rep(0,20))
-hisp <- c(rep(0,20), rep(1,10), rep(0,10))
-api <- c(rep(0,30), rep(1,10))
-male <- rep((c(rep(1,5), rep(0,5))), 4)
-female <- rep((c(rep(0,5), rep(1,5))), 4)
-noHS <- rep(c(1,0,0,0,0),8)
-HSgrad <- rep(c(0,1,0,0,0),8)
-somecollege <- rep(c(0,0,1,0,0),8)
-bachelors <- rep(c(0,0,0,1,0),8)
-adv_degree <- rep(c(0,0,0,0,1),8)
-fake.dataset.2 = data.frame(white, black, hisp, api, male, female, noHS, HSgrad, somecollege, bachelors, adv_degree)
-
-#MASSACHUSETTS
-vote.df20<-vote.df20[,c("rvote", "eth", "sex", "edu")]
-
-var1 = vote.df20$eth
-var1 = as.factor(var1)
-var2 = vote.df20$sex
-var2 = as.factor(var2)
-var3 = vote.df20$edu
-var3 = as.factor(var3)
-
-
-check = glmer(formula = rvote ~ (1|var1) + (1|var2) + (1|var3), data = vote.df20, family = binomial) 
-display(check) 
-
-glmer_predictions = predict(check, newdata = vote.df20, type="response")
-glmer_predictions = round(glmer_predictions, digits = 7) #round for aesthetics and clear comparison
-glmer_predictions = as.data.frame(table(glmer_predictions)) #creates a frequency column to match with gp later
-glmer_predictions = glmer_predictions[order(glmer_predictions$Freq),] #order data frame by frequency
-glmer_predictions
-
-#compare to
-gptest = gp(formula = rvote~rbf(c("sex", "edu", "eth")), data = vote.df20, family = binomial)
-gp_predictions<-predict(gptest, vote.df20, type="response") #run gp through whole data set to get frequencies
-gp_predictions = round(gp_predictions, digits = 7) #round for clear comparison
-gp_predictions = as.data.frame(table(gp_predictions)) #access frequency
-gp_predictions = gp_predictions[order(gp_predictions$Freq),] #order by frequency to match with glmer
-gp_predictions
-=======
-#function takes in as arguments state number and desired sample size. It returns a data set 
-# of random observations of the specified state and size. Plot = 1 includes plots; plots = 0 has no plots.
->>>>>>> 0de3c73b81aeedca73d5f0c08b68e3d4d0a01334
-
 sample_selector = function(state_number, sample_n, plots){ 
   vote_data = read.delim("votingdata.dat") # Read in dataset from .dat file
   vote_data = na.exclude(vote_data) # Remove all entries with missing data
@@ -158,7 +32,7 @@ sample_selector = function(state_number, sample_n, plots){
   sample_data = group[sample(1:length(group$stt), sample_n),]
   sample_data = sample_data[,c("rvote", "eth", "sex", "edu")]
   
-  gp_output<-gp(formula = rvote~rbf(columns = c("sex", "edu", "eth"), l = c(.3, .15, 2.5)), data = sample_data, family = binomial)
+  gp_output<-gp(formula = rvote~rbf(columns = c("sex", "edu", "eth"), l = c(.5, .15, 2)), data = sample_data, family = binomial)
   gp_predictions<-predict(gp_output, sample_data, type="response") 
   ?rbf
   eth = c(rep(1,10), rep(2,10), rep(3,10), rep(4,10))
@@ -251,6 +125,10 @@ sample_selector(state_number = 20, sample_n = 2000, plots = 1) #Mass with plots,
 sample_selector(state_number = 2, sample_n = 1700, plots = 1) #Arizona with plots
 sample_selector(state_number = 4, sample_n = 1950, plots = 1) #california with plots
 
+
+
+
+
 # Reading in & processing of data
 vote_data = read.delim("votingdata.dat") # Read in dataset from .dat file
 vote_data = na.exclude(vote_data) # Remove all entries with missing data
@@ -288,6 +166,3 @@ vote_data$adv_degree <- ifelse(vote_data$edu==5, c(1), c(0))
 # ((possibly working with these variables later))
 vote_data$mar<-ifelse(vote_data$mar==1, c(1), c(0)) # Recode married to 0 1
 vote_data$kid<-ifelse(vote_data$kid==1, c(1), c(0)) # Recode kid to 0 1 
-
-state_number =20
-sample_n = 2000
